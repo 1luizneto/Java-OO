81d7aea7868db26b2b4a728ff61b49399d1a86e8
Luiz Neto <luiznetopb@gmail.com>
2023-07-04 11:50:17 -0300

    Organizando

--- Java Básico - Estrutura sequencial/Teste Saída/Main.java
+++ Java Básico - Estrutura sequencial/Teste Saída/Main.java
@@ -1,6 +1,26 @@
+import java.util.Locale;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("zap");
-        }
+    public static void main(String[] args){
+
+        String nome = "Luiz";
+        int idade = 21;
+        double renda = 1000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais\n", nome, idade, renda);
+
+        /*
+         int y = 32;
+         double x = 10.35745;
+
+         Locale.setDefault(Locale.US);
+         System.out.println("oi");
+         System.out.println("zap");
+         System.out.println(y);
+         System.out.println(x);
+         System.out.printf("%.2f\n", x);
+         System.out.println("Resultado = " + x + " Metros");
+         System.out.printf("Resultado = %.2f Metros\n", x);
+        */
+
     }
+}
