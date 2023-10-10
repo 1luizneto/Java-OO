ed154f9b5f858cfd9b3cbfafc4a17f6fc934d7bf
Luiz Neto <luiznetopb@gmail.com>
2023-10-09 21:06:13 -0300

    Add Ex36

---
+++ Tratamento de exceções/Ex36/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe os dados da conta");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        System.out.print("Titular: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Saldo inicial: ");
+        double balance = sc.nextDouble();
+        System.out.print("Limite de saque: ");
+        double withdrawLimit = sc.nextDouble();
+
+        sc.close();
+
+    }
+}
